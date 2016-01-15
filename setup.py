460ee5877dd25a2e976180c77778072e10bfde43
Michael Milligan <milligan@msi.umn.edu>
2016-01-14 18:13:07 -0600

    Add boilerplate to play nicely with pip and git

---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Juptyer Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+#-----------------------------------------------------------------------------
+# Minimal Python version sanity check (from IPython/Jupyterhub)
+#-----------------------------------------------------------------------------
+
+from __future__ import print_function
+
+import os
+import sys
+
+from distutils.core import setup
+
+pjoin = os.path.join
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the current package version.
+version_ns = {}
+with open(pjoin(here, 'version.py')) as f:
+    exec(f.read(), {}, version_ns)
+
+setup_args = dict(
+    name                = 'remote_user',
+    packages            = ['remote_user'],
+    version             = version_ns['__version__'],
+    description         = """REMOTE_USER Authenticator: An Authenticator for Jupyterhub to read user information from HTTP request headers, as when running behind an authenticating proxy.""",
+    long_description    = "",
+    author              = "Carl (https://github.com/cwaldbieser)",
+    author_email        = "",
+    url                 = "http://jupyter.org",
+    license             = "BSD",
+    platforms           = "Linux, Mac OS X",
+    keywords            = ['Interactive', 'Interpreter', 'Shell', 'Web'],
+    classifiers         = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
+
+# setuptools requirements
+if 'setuptools' in sys.modules:
+    setup_args['install_requires'] = install_requires = []
+    with open('requirements.txt') as f:
+        for line in f.readlines():
+            req = line.strip()
+            if not req or req.startswith(('-e', '#')):
+                continue
+            install_requires.append(req)
+
+
+def main():
+    setup(**setup_args)
+
+if __name__ == '__main__':
+    main()
