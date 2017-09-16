70ddc832ae495cad6d2b2b9ee90e181091660ef4
Carl Waldbieser <cwaldbieser@gmail.com>
2017-09-16 16:52:58 -0400

    Added wheel for python3.

---
+++ build/lib/jhub_remote_user_authenticator/remote_user_auth.py
@@ -0,0 +1,61 @@
+
+import os
+from jupyterhub.handlers import BaseHandler
+from jupyterhub.auth import Authenticator
+from jupyterhub.auth import LocalAuthenticator
+from jupyterhub.utils import url_path_join
+from tornado import gen, web
+from traitlets import Unicode
+
+
+class RemoteUserLoginHandler(BaseHandler):
+
+    def get(self):
+        header_name = self.authenticator.header_name
+        remote_user = self.request.headers.get(header_name, "")
+        if remote_user == "":
+            raise web.HTTPError(401)
+        else:
+            user = self.user_from_username(remote_user)
+            self.set_login_cookie(user)
+            self.redirect(url_path_join(self.hub.server.base_url, 'home'))
+
+
+class RemoteUserAuthenticator(Authenticator):
+    """
+    Accept the authenticated user name from the REMOTE_USER HTTP header.
+    """
+    header_name = Unicode(
+        default_value='REMOTE_USER',
+        config=True,
+        help="""HTTP header to inspect for the authenticated username.""")
+
+    def get_handlers(self, app):
+        return [
+            (r'/login', RemoteUserLoginHandler),
+        ]
+
+    @gen.coroutine
+    def authenticate(self, *args):
+        raise NotImplementedError()
+
+
+class RemoteUserLocalAuthenticator(LocalAuthenticator):
+    """
+    Accept the authenticated user name from the REMOTE_USER HTTP header.
+    Derived from LocalAuthenticator for use of features such as adding
+    local accounts through the admin interface.
+    """
+    header_name = Unicode(
+        default_value='REMOTE_USER',
+        config=True,
+        help="""HTTP header to inspect for the authenticated username.""")
+
+    def get_handlers(self, app):
+        return [
+            (r'/login', RemoteUserLoginHandler),
+        ]
+
+    @gen.coroutine
+    def authenticate(self, *args):
+        raise NotImplementedError()
